478d06a5dc819a8953fa379ae369bba89e78b79f
TonyAng <13323650885@163.com>
2016-11-04 10:13:12 +0800

    Initial Commit

---
+++ YTPageControlDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YTPageControlDemo
+//
+//  Created by TonyAng on 16/11/4.
+//  Copyright © 2016年 TonyAng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YTPageControlDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YTPageControlDemo
+//
+//  Created by TonyAng on 16/11/4.
+//  Copyright © 2016年 TonyAng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
